37c1f07bcfa9a35e03cc295ee59388141f1c761b
Teresa Lok <a610tl@gmail.com>
2024-10-07 19:12:52 -0600

    Lab Assignment 2: Layout Components

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+import React from "react";
+import { SafeAreaView } from "react-native";
+import ToDoForm from "./screen/todoform";
+import ToDoList from "./screen/todolist";
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <ToDoForm />
+      <ToDoList />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
